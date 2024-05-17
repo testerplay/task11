aff1dfe22a256a6b4124068ce719d1e41adbea1d
kulag <kulag@DESKTOP-CH7J89A>
2024-05-18 01:23:17 +0300

    Добавьте файлы проекта.

---
+++ task11/task11.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int N;
+    std::cout << "Введіть кількість секунд N: ";
+    std::cin >> N;
+
+    // Знайти кількість секунд, що пройшли з початку останньої хвилини
+    int seconds_in_last_minute = N % 60;
+
+    std::cout << "Кількість секунд, що пройшли з початку останньої хвилини: " << seconds_in_last_minute << std::endl;
+
+    return 0;
+}
